ef2b414740e23685ffa2696a0ad9f6a80ef53968
disco-rocket <52636251+disco-rocket@users.noreply.github.com>
2019-08-17 08:12:26 +0100

    Added doc strings to data_pipeline.py

--- data/process_data.py
+++ data/process_data.py
@@ -3,11 +3,21 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Input:  Takes the fle paths of the two CSV files. The message filepath must come first, followed by the categories one.
+    Output: The merged CSV files
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     return messages.merge(categories, how='left',left_on='id',right_on='id')
 
 def clean_data(df):
+    """
+    Purpose: This takes the categories column and creates binary columns for each category.
+             It also removes duplicates
+    Input: the merged message and category dataframe
+    Output: the cleaned dataframe
+    """
     #splitting out the category values into different columns
     categories = df['categories'].str.split(';', expand=True)
     #getting category headers
@@ -42,12 +52,20 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+    """
+    Purpose: This saves a dataframe in the specified database. Nothing is returned.
+    Input: 1) The dataframe you want to save
+           2) The name of the database to save it into
+    """
     #example database_filename 'disaster_project.db'
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('clean_data', engine, index=False, if_exists='replace')  
 
 
 def main():
+    """
+    Main function to call other functions.
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
