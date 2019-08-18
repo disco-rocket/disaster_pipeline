7990e27cd5c70341eb5a1b2e7ae71b9f66b9e6e6
Sean Kiely <sean.p.kiely@gmail.com>
2019-08-18 08:46:02 +0100

    Adding back previously deleted code and adding screenshots

--- disasterapp/run.py
+++ disasterapp/run.py
@@ -6,6 +6,19 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar, Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 app = Flask(__name__)
 
